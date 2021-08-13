350c573f065a05c608005cce1fc823606d2caabf
Everton-ZUP <everton.fabris@zup.com.br>
2021-08-13 18:41:06 -0300

    Adicionado método de editar e excluir

--- src/main/java/br/zup/seguranca/Login/controller/ControllerUsuario.java
+++ src/main/java/br/zup/seguranca/Login/controller/ControllerUsuario.java
@@ -4,14 +4,17 @@ package br.zup.seguranca.Login.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +54,21 @@ public class ControllerUsuario {
 	@GetMapping("/{id}")
 	public UsuarioDTO detalhar(@PathVariable Long id) {
 		return new UsuarioDTO(repositoryUsuario.findById(id));
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<UsuarioDTO> Atualizar(@PathVariable Long id, @RequestBody @Valid Usuario usr, UriComponentsBuilder uribuilder) {
+		usr.setID(id);
+		repositoryUsuario.save(usr);
+		URI uri = uribuilder.path("/usuario/{id}").buildAndExpand(usr.getID()).toUri();
+		return ResponseEntity.created(uri).body(new UsuarioDTO(usr));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deletar(@PathVariable Long id){
+		repositoryUsuario.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
